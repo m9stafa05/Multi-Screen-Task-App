5d02b32cdcc58cf84843b8d16e5d12eeee2dc294
m9stafa05 <m9stafa05@gmail.com>
2025-07-18 05:56:08 +0300

    Add Account Setup to take data

---
+++ lib/features/account_setup/presentation/views/account_setup_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:multi_screen_task_app/features/account_setup/presentation/views/widgets/account_setup_view_body.dart';
+
+class AccountSetupView extends StatelessWidget {
+  const AccountSetupView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: AccountSetupViewBody());
+  }
+}
---
+++ lib/features/account_setup/presentation/views/widgets/account_setup_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AccountSetupViewBody extends StatelessWidget {
+  const AccountSetupViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: const Text("Acc"));
+  }
+}
