bfe54550d390ed5a13704b9176690cc5af9f902c
m9stafa05 <m9stafa05@gmail.com>
2025-07-18 05:53:58 +0300

    add router and provide the cubit

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,14 +1,25 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:multi_screen_task_app/features/account_setup/presentation/views/account_setup_view.dart';
 import 'package:multi_screen_task_app/features/home/presentation/views/home_view.dart';
+import 'package:multi_screen_task_app/features/splash/presentation/controllers/user_data_cubit/user_data_cubit.dart';
 import 'package:multi_screen_task_app/features/splash/presentation/views/splash_view.dart';
 
 abstract class AppRouter {
   static const String kHomeView = '/home_view';
+  static const String kAccountSetup = '/account_setup_view';
   static GoRouter router = GoRouter(
     routes: [
       GoRoute(
         path: '/',
-        builder: (context, state) => const SplashView(),
+        builder: (context, state) => BlocProvider(
+          create: (context) => UserDataCubit(),
+          child: const SplashView(),
+        ),
+      ),
+      GoRoute(
+        path: kAccountSetup,
+        builder: (context, state) => const AccountSetupView(),
       ),
       GoRoute(
         path: kHomeView,
