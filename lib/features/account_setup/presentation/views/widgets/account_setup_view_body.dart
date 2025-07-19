4acfd5ab65ec89cb5bcca0c854763c47c8a513ab
m9stafa05 <m9stafa05@gmail.com>
2025-07-19 07:18:55 +0300

    Add Navigate button to form view

--- lib/features/account_setup/presentation/views/widgets/account_setup_view_body.dart
+++ lib/features/account_setup/presentation/views/widgets/account_setup_view_body.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:multi_screen_task_app/core/utils/app_router.dart';
+import 'package:multi_screen_task_app/features/account_setup/presentation/views/widgets/set_up_button.dart';
 
 class AccountSetupViewBody extends StatelessWidget {
   const AccountSetupViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: const Text("Acc"));
+    return Center(
+      child: SetUpButton(
+        onPressed: () {
+          GoRouter.of(context).go(AppRouter.kFormDataView);
+        },
+      ),
+    );
   }
 }
