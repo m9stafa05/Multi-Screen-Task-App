a4a0afcb4be0190cf4293d33b293358fc37d5444
m9stafa05 <m9stafa05@gmail.com>
2025-07-19 09:37:59 +0300

    Use listener to handle navigation and failed state & Save USer  data

--- lib/features/account_setup/presentation/views/widgets/account_set_up_form.dart
+++ lib/features/account_setup/presentation/views/widgets/account_set_up_form.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:multi_screen_task_app/core/utils/app_router.dart';
+import 'package:multi_screen_task_app/features/account_setup/presentation/controllers/account_set_up_cubit/account_set_up_cubit.dart';
 import 'package:multi_screen_task_app/features/account_setup/presentation/views/widgets/custom_button.dart';
 import 'package:multi_screen_task_app/features/account_setup/presentation/views/widgets/custom_form_text_field.dart';
 import 'package:multi_screen_task_app/features/account_setup/presentation/views/widgets/gender_drop_down.dart';
 import 'package:multi_screen_task_app/features/account_setup/presentation/views/widgets/profile_image_picer.dart';
+
 
 class AccountSetUpForm extends StatefulWidget {
   const AccountSetUpForm({super.key});
@@ -22,44 +27,67 @@ class _AccountSetUpFormState extends State<AccountSetUpForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: ListView(
-        children: [
-          const SizedBox(height: 16),
-          const Text(
-            textAlign: TextAlign.center,
-            'Set Up Your Account',
-            style: TextStyle(color: Colors.white, fontSize: 30),
-          ),
-          const SizedBox(height: 16),
-          // First Name
-          CustomFormTextField(
-            controller: firstNameController,
-            labelText: 'First Name',
-          ),
-          const SizedBox(height: 16),
+    return BlocListener<AccountSetUpCubit, AccountSetUpState>(
+      listener: (context, state) {
+        if (state is AccountSetUpSaved) {
+          GoRouter.of(context).go(AppRouter.kHomeView);
+        } else if (state is AccountSetUpFailed) {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text(state.errorMessage)));
+        }
+      },
+      child: Form(
+        key: _formKey,
+        child: ListView(
+          children: [
+            const SizedBox(height: 16),
+            const Text(
+              textAlign: TextAlign.center,
+              'Set Up Your Account',
+              style: TextStyle(color: Colors.white, fontSize: 30),
+            ),
+            const SizedBox(height: 16),
+            // First Name
+            CustomFormTextField(
+              controller: firstNameController,
+              labelText: 'First Name',
+            ),
+            const SizedBox(height: 16),
 
-          // Last Name
-          CustomFormTextField(
-            controller: lastNameController,
-            labelText: 'Last Name',
-          ),
-          const SizedBox(height: 16),
+            // Last Name
+            CustomFormTextField(
+              controller: lastNameController,
+              labelText: 'Last Name',
+            ),
+            const SizedBox(height: 16),
 
-          // Gender Dropdown
-          const GenderDropdown(),
+            // Gender Dropdown
+            const GenderDropdown(),
 
-          const SizedBox(height: 16),
+            const SizedBox(height: 16),
 
-          // Optional Image ()
-          const ProfileImagePicker(),
+            // Optional Image ()
+            const ProfileImagePicker(),
 
-          const SizedBox(height: 24),
+            const SizedBox(height: 24),
 
-          // Submit Button
-          CustomButton(text: 'Finish', onPressed: () {}),
-        ],
+            // Submit Button
+            CustomButton(
+              text: 'Finish',
+              onPressed: () {
+                final cubit = context.read<AccountSetUpCubit>();
+                if (_formKey.currentState!.validate()) {
+                  cubit.saveUserData(
+                    firstName: firstNameController.text,
+                    lastName: lastNameController.text,
+                    gender: cubit.gender ?? 'Other',
+                  );
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
