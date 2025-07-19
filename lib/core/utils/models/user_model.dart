d385910c43685ff9dc92593374c1863ebf31f2c6
m9stafa05 <m9stafa05@gmail.com>
2025-07-19 07:58:07 +0300

    Update user Model to take image

--- lib/core/utils/models/user_model.dart
+++ lib/core/utils/models/user_model.dart
@@ -2,26 +2,33 @@ class UserModel {
   final String firstName;
   final String lastName;
   final String gender;
+  final String? image;
+
 
   UserModel({
+    this.image,
     required this.firstName,
     required this.lastName,
     required this.gender,
   });
+
 
   Map<String, String> toJson() {
     return {
       'firstName': firstName,
       'lastName': lastName,
       'gender': gender,
+      'image': image ?? '',
     };
   }
+
 
   factory UserModel.fromJson(Map<String, dynamic> map) {
     return UserModel(
       firstName: map['firstName'] ?? '',
       lastName: map['lastName'] ?? '',
       gender: map['gender'] ?? '',
+      image: map['image'] == '' ? null : map['image'],
     );
   }
 }
