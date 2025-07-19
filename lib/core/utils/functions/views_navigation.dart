b26e422c96cfcd790b03e117810534cef9a364af
m9stafa05 <m9stafa05@gmail.com>
2025-07-19 22:44:32 +0300

    Update method location access

--- lib/core/utils/functions/views_navigation.dart
+++ lib/core/utils/functions/views_navigation.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 void navigateIfNeeded(BuildContext context, String targetRoute) {
-  final current = GoRouter.of(
+final current = GoRouter.of(
     context,
-  ).routerDelegate.currentConfiguration.fullPath;
+  ).routeInformationProvider.value.uri.toString();
+
   if (current != targetRoute) {
     GoRouter.of(context).go(targetRoute);
+    Navigator.pop(context);
   } else {
     Navigator.pop(context);
   }
