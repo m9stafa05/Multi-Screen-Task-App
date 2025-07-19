8abd91e0005a169232d41500e7fd0160e01196d7
m9stafa05 <m9stafa05@gmail.com>
2025-07-19 22:45:26 +0300

    Update On tap function

--- lib/core/widgets/custom_bottom_navigation_bar.dart
+++ lib/core/widgets/custom_bottom_navigation_bar.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:multi_screen_task_app/core/utils/app_router.dart';
 
 class CustomBottomNavBar extends StatelessWidget {
-  const CustomBottomNavBar({super.key});
-
+  const CustomBottomNavBar({super.key, required this.currentIndex});
+  final int currentIndex;
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      currentIndex: 0, // must be set to avoid errors
+      currentIndex: currentIndex, // must be set to avoid errors
       onTap: (index) {
-        // Handle navigation here
+        switch (index) {
+          case 0:
+            context.go(AppRouter.kHomeView);
+            break;
+          case 1:
+            context.go(AppRouter.kTaskView);
+            break;
+          case 2:
+            context.go(AppRouter.kProfileView);
+            break;
+        }
       },
       items: const [
         BottomNavigationBarItem(
