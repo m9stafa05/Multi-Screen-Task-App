673a8b2c256100d6b43751daef80c99b0545a230
m9stafa05 <m9stafa05@gmail.com>
2025-07-19 09:05:23 +0300

    Rename amd add const

--- lib/features/account_setup/presentation/views/widgets/account_setup_view_body.dart
+++ lib/features/account_setup/presentation/views/widgets/account_setup_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:multi_screen_task_app/core/utils/app_router.dart';
-import 'package:multi_screen_task_app/features/account_setup/presentation/views/widgets/set_up_button.dart';
+import 'package:multi_screen_task_app/features/account_setup/presentation/views/widgets/starter_button.dart';
 
 class AccountSetupViewBody extends StatelessWidget {
   const AccountSetupViewBody({super.key});
@@ -9,7 +9,7 @@ class AccountSetupViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: SetUpButton(
+      child: StarterButton(
         onPressed: () {
           GoRouter.of(context).go(AppRouter.kFormDataView);
         },
--- lib/features/account_setup/presentation/views/widgets/form_data_view_body.dart
+++ lib/features/account_setup/presentation/views/widgets/form_data_view_body.dart
@@ -1,19 +1,14 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:multi_screen_task_app/features/account_setup/presentation/views/widgets/account_set_up_form.dart';
-import 'package:multi_screen_task_app/features/account_setup/presentation/views/widgets/custom_form_text_field.dart';
 
 class FormDataViewBody extends StatelessWidget {
   const FormDataViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: AccountSetUpForm()
+    return const Padding(
+      padding: EdgeInsets.all(16.0),
+      child: AccountSetUpForm(),
     );
   }
 }
-
