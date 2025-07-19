27d9a195c5fb0d8c4545893bf99d1705645d1e21
m9stafa05 <m9stafa05@gmail.com>
2025-07-19 21:46:10 +0300

    Fix import

--- lib/core/utils/shared_pref.dart
+++ lib/core/utils/shared_pref.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 
-import 'package:multi_screen_task_app/core/utils/models/user_model.dart';
+import 'package:multi_screen_task_app/core/models/user_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class UserPreferences {
