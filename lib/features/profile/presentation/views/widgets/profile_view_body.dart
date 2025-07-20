af51a0e87b3969e745eaf791f4c9d6692bc9ed4c
m9stafa05 <m9stafa05@gmail.com>
2025-07-20 23:13:49 +0300

    Start building

--- lib/features/profile/presentation/views/widgets/profile_view_body.dart
+++ lib/features/profile/presentation/views/widgets/profile_view_body.dart
@@ -1,10 +1,65 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class ProfileViewBody extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:multi_screen_task_app/core/utils/shared_pref.dart';
+
+class ProfileViewBody extends StatefulWidget {
   const ProfileViewBody({super.key});
 
   @override
+  State<ProfileViewBody> createState() => _ProfileViewBodyState();
+}
+
+class _ProfileViewBodyState extends State<ProfileViewBody> {
+  String? userImage;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
+
+  Future<void> _loadUserData() async {
+    final user = await UserPreferences.loadUser();
+    if (user != null) {
+      setState(() {
+        userImage = user.image;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Column();
+    return  Column(
+  children: [
+    ProfileInfoImage(
+      userImage: userImage ?? 'assets/profile.png',
+    ),
+  ],
+);
+  }
+}
+
+class ProfileInfoImage extends StatelessWidget {
+  const ProfileInfoImage({super.key, required this.userImage});
+  final String userImage;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          width: 150,
+          height: 150,
+          child: CircleAvatar(
+            radius: 60,
+            backgroundImage:
+                userImage != null && File(userImage!).existsSync()
+                ? FileImage(File(userImage!))
+                : const AssetImage('assets/profile.png')
+                      as ImageProvider,
+          ),
+        ),
+      ],
+    );
   }
 }
