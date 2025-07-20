67254bc770e1650551cf460f2eae30f40b462d3c
m9stafa05 <m9stafa05@gmail.com>
2025-07-20 06:28:33 +0300

    Update to svave data and load it

--- lib/features/tasks/presentation/controllers/task_cubit/task_cubit.dart
+++ lib/features/tasks/presentation/controllers/task_cubit/task_cubit.dart
@@ -1,16 +1,32 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:multi_screen_task_app/features/tasks/data/model/task_model.dart';
+import 'package:multi_screen_task_app/features/tasks/data/storage/task_storage.dart';
 
 part 'task_state.dart';
 
 class TaskCubit extends Cubit<TaskState> {
-  TaskCubit() : super(TaskInitial());
   final List<TaskModel> tasks = [];
+  TaskCubit() : super(TaskAdded(tasks: const [])) {
+    loadTasks();
+  }
+
+  Future<void> loadTasks() async {
+    final loadedTasks = await TaskStorage.loadTasks();
+    tasks
+      ..clear()
+      ..addAll(loadedTasks);
+    emit(TaskAdded(tasks: List.from(tasks)));
+  }
+
+  Future<void> saveTasks() async {
+    await TaskStorage.saveTasks(tasks);
+  }
 
   void addTask(String title) {
     tasks.add(TaskModel(title: title));
     emit(TaskAdded(tasks: List.from(tasks)));
+    saveTasks();
   }
 
   void toggleTask(TaskModel task) {
@@ -18,12 +34,14 @@ class TaskCubit extends Cubit<TaskState> {
     if (index != -1) {
       tasks[index] = task.copyWith(isCompleted: !task.isCompleted);
       emit(TaskAdded(tasks: List.from(tasks)));
+      saveTasks();
     }
   }
 
   void removeTask(int index) {
     tasks.removeAt(index);
     emit(TaskAdded(tasks: List.from(tasks)));
+    saveTasks();
   }
 
   List<TaskModel> get incompleteTasks =>
--- lib/features/tasks/presentation/controllers/task_cubit/task_state.dart
+++ lib/features/tasks/presentation/controllers/task_cubit/task_state.dart
@@ -3,8 +3,6 @@ part of 'task_cubit.dart';
 @immutable
 sealed class TaskState {}
 
-final class TaskInitial extends TaskState {}
-
 final class TaskAdded extends TaskState {
   final List<TaskModel> tasks;
 
