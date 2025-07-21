f53427461ad6009374c537f8620cf31be4f57118
m9stafa05 <m9stafa05@gmail.com>
2025-07-22 00:00:50 +0300

    Refactor account setup view

--- lib/features/account_setup/presentation/views/widgets/account_setup_view_body.dart
+++ lib/features/account_setup/presentation/views/widgets/account_setup_view_body.dart
@@ -9,10 +9,30 @@ class AccountSetupViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: StarterButton(
-        onPressed: () {
-          GoRouter.of(context).go(AppRouter.kFormDataView);
-        },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(
+            Icons.account_circle,
+            size: 80,
+            color: Colors.blueAccent,
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            "Set Up Your Account",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1.2,
+            ),
+          ),
+          const SizedBox(height: 20),
+          StarterButton(
+            onPressed: () {
+              GoRouter.of(context).go(AppRouter.kFormDataView);
+            },
+          ),
+        ],
       ),
     );
   }
