b94e47876aad1f168de33ffa90fd40b21b07bd04
m9stafa05 <m9stafa05@gmail.com>
2025-07-21 04:52:23 +0300

    Update user model to store tasks count

--- lib/core/models/user_model.dart
+++ lib/core/models/user_model.dart
@@ -3,15 +3,35 @@ class UserModel {
   final String lastName;
   final String gender;
   final String? image;
-
+  final int completeCount;
+  final int unCompleteCount;
 
   UserModel({
     this.image,
     required this.firstName,
     required this.lastName,
     required this.gender,
+    this.completeCount = 0,
+    this.unCompleteCount = 0,
   });
 
+  UserModel copyWith({
+    String? firstName,
+    String? lastName,
+    String? gender,
+    String? image,
+    int? completeCount,
+    int? unCompleteCount,
+  }) {
+    return UserModel(
+      firstName: firstName ?? this.firstName,
+      lastName: lastName ?? this.lastName,
+      gender: gender ?? this.gender,
+      image: image ?? this.image,
+      completeCount: completeCount ?? this.completeCount,
+      unCompleteCount: unCompleteCount ?? this.unCompleteCount,
+    );
+  }
 
   Map<String, String> toJson() {
     return {
@@ -19,9 +39,10 @@ class UserModel {
       'lastName': lastName,
       'gender': gender,
       'image': image ?? '',
+      'completeCount': completeCount.toString(),
+      'unCompleteCount': unCompleteCount.toString(),
     };
   }
-
 
   factory UserModel.fromJson(Map<String, dynamic> map) {
     return UserModel(
@@ -29,6 +50,9 @@ class UserModel {
       lastName: map['lastName'] ?? '',
       gender: map['gender'] ?? '',
       image: map['image'] == '' ? null : map['image'],
+      completeCount: int.tryParse(map['completeCount'] ?? '0') ?? 0,
+      unCompleteCount:
+          int.tryParse(map['unCompleteCount'] ?? '0') ?? 0,
     );
   }
 }
