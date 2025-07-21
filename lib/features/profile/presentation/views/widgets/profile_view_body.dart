361ff5366efa1657dd7409f6397812ff13b93bd9
m9stafa05 <m9stafa05@gmail.com>
2025-07-21 23:53:33 +0300

    Build Delete Account Button

--- lib/features/profile/presentation/views/widgets/profile_view_body.dart
+++ lib/features/profile/presentation/views/widgets/profile_view_body.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:multi_screen_task_app/core/controllers/cubit/user_info_cubit.dart';
+import 'package:multi_screen_task_app/core/utils/app_router.dart';
+import 'package:multi_screen_task_app/core/utils/shared_pref.dart';
 import 'package:multi_screen_task_app/features/profile/presentation/views/widgets/profile_info_image.dart';
 import 'package:multi_screen_task_app/features/profile/presentation/views/widgets/task_score.dart';
 
@@ -20,6 +23,25 @@ class ProfileViewBody extends StatelessWidget {
                   ProfileInfoSection(user: userInfo),
                   const SizedBox(height: 60),
                   TasksScore(userModel: userInfo),
+                  const SizedBox(height: 180),
+                  ElevatedButton(
+                    onPressed: () {
+                      UserPreferences.logout();
+                      GoRouter.of(context).go(AppRouter.kSplashView);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 12,
+                      ),
+                      backgroundColor: Colors.red,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: const Text('Delete Account'),
+                  ),
                 ],
               );
             } else if (state is UserInfoFailed) {
