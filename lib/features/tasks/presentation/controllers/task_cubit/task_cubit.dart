52b83bd104ffac88de521e569b5ef95ad6a6333a
m9stafa05 <m9stafa05@gmail.com>
2025-07-21 05:05:53 +0300

    provide Count for tasks

--- lib/features/tasks/presentation/controllers/task_cubit/task_cubit.dart
+++ lib/features/tasks/presentation/controllers/task_cubit/task_cubit.dart
@@ -1,13 +1,16 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:multi_screen_task_app/features/profile/presentation/controllers/cubit/user_info_cubit.dart';
 import 'package:multi_screen_task_app/features/tasks/data/model/task_model.dart';
 import 'package:multi_screen_task_app/features/tasks/data/storage/task_storage.dart';
 
 part 'task_state.dart';
 
 class TaskCubit extends Cubit<TaskState> {
+  final UserInfoCubit userCubit;
   final List<TaskModel> tasks = [];
-  TaskCubit() : super(TaskAdded(tasks: const [])) {
+  TaskCubit({required this.userCubit})
+    : super(TaskAdded(tasks: const [])) {
     loadTasks();
   }
 
@@ -25,6 +28,7 @@ class TaskCubit extends Cubit<TaskState> {
 
   void addTask(String title) {
     tasks.add(TaskModel(title: title));
+    userCubit.incrementUnComplete();
     emit(TaskAdded(tasks: List.from(tasks)));
     saveTasks();
   }
@@ -32,7 +36,9 @@ class TaskCubit extends Cubit<TaskState> {
   void toggleTask(TaskModel task) {
     final index = tasks.indexOf(task);
     if (index != -1) {
+      final isNowComplete = !task.isCompleted;
       tasks[index] = task.copyWith(isCompleted: !task.isCompleted);
+      userCubit.toggleTaskScore(isNowComplete);
       emit(TaskAdded(tasks: List.from(tasks)));
       saveTasks();
     }
